9d6c53084762d87408a09adfbeb30d00db71dc47
Marcelo Mollinari <mmollina@gmail.com>
2020-12-29 23:45:20 -0500

    update vcf2sm script

---
+++ vcf2sm/test.sh
@@ -0,0 +1,16 @@
+python2.7 VCF2SM.py -i ../data/sample_vcf/chr3.vcf \
+                 -o trifida_sm_chr3.vcf \
+                 -d 20 \
+                 -D 2000 \
+                 -a AD \
+                 -g BT \
+                 -1 Beauregard \
+                 -2 Tanzania \
+                 -S ~/repos/supermassa/src/SuperMASSA.py \
+                 -I f1 \
+                 -M 2:6 \
+                 -f 6 \
+                 -p 0.80 \
+                 -n 0.90 \
+                 -c 0.75 \
+                 -t 4
