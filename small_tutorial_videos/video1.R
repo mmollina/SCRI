8960ba7f40c99773318cc967e50b9ae083be2390
Marcelo Mollinari <mmollina@gmail.com>
2022-01-13 10:58:46 -0500

    delete unused folder

--- small_tutorial_videos/video1.R
+++
@@ -1,9 +0,0 @@
-require(mappoly)
-setwd("~/repos/SCRI/small_tutorial_videos/")
-dat <- read_fitpoly(file.in = "B2721_scores.dat", 
-                    ploidy = 4,
-                    parent1 = "Atlantic", 
-                    parent2 = "B1829", 
-                    verbose = TRUE)
-plot(dat)
-print(dat, detailed = TRUE)
